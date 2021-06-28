0b4b551f4d3e64cdaf5b52f9fff31b1f624a2d86
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-06-28 12:32:02 -0400

    Add some instruction

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
 MainHandler::LoadSentry();
 std::cout << "Type the string to input: ";
 std::getline(std::cin, userinput);
-sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,"Printed Result",userinput.c_str()));
+sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,"User Input",userinput.c_str()));
 std::cout << ("Your Input: "+userinput+"\n").c_str();
 std::cout << "\nPress enter to close the software...";
 std::getline(std::cin, userinput);
