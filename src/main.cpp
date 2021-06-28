832cc102be53582a0e7121a84f2037ac492aaecf
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-06-28 12:23:27 -0400

    Some internal updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <main.h>
 
-std::string test = "Some Test Message";
+std::string userinput;
 int main() {
 MainHandler::LoadSentry();
-sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,"Printed Result",test.c_str()));
-std::cout << test.c_str();
+std::cout << "Type the string to input: ";
+std::getline(std::cin, userinput);
+sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,"Printed Result",userinput.c_str()));
+std::cout << ("Your Input: "+userinput+"\n").c_str();
+std::cout << "\nPress enter to close the software...";
+std::getline(std::cin, userinput);
 sentry_close();
 return 0;
 }
