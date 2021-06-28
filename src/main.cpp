f7094c335a8a2f52834eba0bb525af39d083d747
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-06-28 11:29:16 -0400

    Some final additional changes for now

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main() {
 MainHandler::LoadSentry();
 sentry_capture_event(sentry_value_new_message_event(SENTRY_LEVEL_INFO,"Printed Result",test.c_str()));
 std::cout << test.c_str();
+sentry_close();
 return 0;
 }
