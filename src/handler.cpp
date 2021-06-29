c388a8e79451971126f409725bbf78078dc9ebb8
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-06-29 00:31:27 -0400

    forgot git add

---
+++ src/handler.cpp
@@ -0,0 +1,9 @@
+#include <handler.h>
+#include <sentry.h>
+
+void MainHandler::LoadSentry() {
+  sentry_options_t *options = sentry_options_new();
+  sentry_options_set_dsn(options, "https://e96a522911724a0f908227eceb3936f4@o125145.ingest.sentry.io/5834480");
+  //sentry_options_set_release(options, "my-project-name@2.3.12");
+  sentry_init(options);
+}
---
+++ src/handler.h
@@ -0,0 +1,3 @@
+namespace MainHandler {
+void LoadSentry();
+}
